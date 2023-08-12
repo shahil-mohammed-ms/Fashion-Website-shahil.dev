38fb8faede1a75f5e4c3f76f5af31b72e0f85485
shahil-mohammed-ms <115145954+shahil-mohammed-ms@users.noreply.github.com>
2023-08-12 11:45:43 +0530

    initial commit

---
+++ server/Route/Admin.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
---
+++ server/src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+require("../src/db/Mongoose/mongoose");
+const cors = require("cors");
+
+
+const app = express();
+app.use(
+  cors({   
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+); 
+
+app.use(bodyParser.json());
+
+const AuthRouter = require('../Route/Auth')
+const AdminRouter = require('../Route/Admin')
+const UserRouter = require('../Route/User')
+
+
+app.use("/image", express.static(path.join(__dirname, "../Public")));
+
+app.use('/',AuthRouter)
+app.use('/Admin',AdminRouter)
+app.use('User',UserRouter)
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
+
+module.exports = app;
