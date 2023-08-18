5f5bffe691c2421dc135af179268b8c1a6303fc1
shahil-mohammed-ms <115145954+shahil-mohammed-ms@users.noreply.github.com>
2023-08-18 12:39:03 +0530

    schema product

--- server/src/db/Models/CartSchema.js
+++ server/src/db/Models/CartSchema.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose')
 
 const CartSchema = new mongoose.Schema({
 
-
+productId:{
+  type:mongoose.Schema.Types.ObjectId
+},
+buyerId:{
+  type:mongoose.Schema.Types.ObjectId
+},
+quantity:{
+  type:Number,
+  
+},
+createdAt: { type: Date, default: Date.now }
 
 })
 
