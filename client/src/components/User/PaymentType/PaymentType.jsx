9bdeebe0e4467132d0603729dfe94e404fdbe4d5
shahil-mohammed-ms <115145954+shahil-mohammed-ms@users.noreply.github.com>
2023-10-09 18:35:12 +0530

    payment  ui

--- client/src/components/User/PaymentType/PaymentType.jsx
+++ client/src/components/User/PaymentType/PaymentType.jsx
@@ -1,21 +1,130 @@
+// import React, { useState, useEffect } from 'react';
+// import { useNavigate,useLocation, Navigate } from 'react-router-dom';
+// import OnlinePayment from './OnlinePayment';
+// import { Button } from '@mui/material';
+// import axios from '../../../axios'
+
+// function PaymentType() {
+//   const navigate = useNavigate();
+//  const location = useLocation();
+//   const [selectedOption, setSelectedOption] = useState('cod');
+//   const[proId,setProId] = useState(null)
+//   const[userId,setUserId] = useState(null)
+//   const[cartId,setCartId] = useState(null)
+//   const[quantity,setQuantity] = useState(null)
+//   const[total,setTotal] = useState(null)
+//   const[name,setName] = useState(null)
+//   const [size,setSize] = useState(null)
+//   const [sellerId,setSellerId] = useState(null)
+//   const [paymentInfo, setPaymentInfo] = useState({
+//     proId: null,
+//     userId: null,
+//     cartId: null,
+//     quantity: null,
+//     total: null,
+//     name: null,
+//     size: null,
+//     sellerId: null,
+//   });
+  
+
+//   useEffect(() => {
+//     // Move your data fetching logic into the useEffect hook
+//     const fetchData = async () => {
+//       const queryParams = new URLSearchParams(location.search);
+//       setProId(queryParams.get('proId'));
+//       setUserId(queryParams.get('userId'));
+//       setCartId(queryParams.get('cartId'));
+//       setQuantity(queryParams.get('quantity'));
+//       setTotal(queryParams.get('total'));
+//       setName(queryParams.get('name'));
+//       setSize(queryParams.get('size'));
+//       setSellerId(queryParams.get('sellerId'));
+
+
+
+//     };
+
+//     fetchData(); // Call the fetchData function when the component mounts
+//   }, [location.search]);
+//   useEffect(() => {
+//     // Set the paymentInfo object based on the existing state variables
+//     setPaymentInfo({
+//       proId,
+//       userId,
+//       cartId,
+//       quantity,
+//       total,
+//       name,
+//       size,
+//       sellerId,
+//     });
+//   }, [proId, userId, cartId, quantity, total, name, size, sellerId]);
+
+// //cod payment
+// const handleCodBuy = async()=>{
+
+//   const response = await axios.post('/Order/addorder',{
+//     COD:'COD',
+//     proId,
+//     userId,
+//     cartId,
+//     quantity,
+//     total,
+//     name,
+//     size,
+//     sellerId
+
+//   })
+//   if(response.data.success){
+
+//     navigate('/UserHome')
+//   }else{
+//     navigate('/cart')
+  
+//   }
+// }
+
+//   const handleOptionChange = (e) => {
+//     setSelectedOption(e.target.value);
+//   };
+//   return (
+//     <div>
+//     <label>
+//       <input
+//         type="radio"
+//         value="cod"
+//         checked={selectedOption === 'cod'}
+//         onChange={handleOptionChange}
+//       />
+//       Cod
+//     </label>
+
+//     <label>
+//       <input
+//         type="radio"
+//         value="online"
+//         checked={selectedOption === 'online'}
+//         onChange={handleOptionChange}
+//       />
+//       Online
+//     </label>
+//     {selectedOption === 'online' ? (<OnlinePayment paymentInfo={paymentInfo} />):(<button onClick={handleCodBuy}>Buy COD</button>)}
+//   </div>
+//   )
+// }
+
+// export default PaymentType
 import React, { useState, useEffect } from 'react';
-import { useNavigate,useLocation, Navigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import OnlinePayment from './OnlinePayment';
-import { Button } from '@mui/material';
-import axios from '../../../axios'
+import { Button, Radio, FormControlLabel, RadioGroup, Typography, Container, Grid } from '@mui/material';
+import axios from '../../../axios';
 
 function PaymentType() {
   const navigate = useNavigate();
- const location = useLocation();
+  const location = useLocation();
   const [selectedOption, setSelectedOption] = useState('cod');
-  const[proId,setProId] = useState(null)
-  const[userId,setUserId] = useState(null)
-  const[cartId,setCartId] = useState(null)
-  const[quantity,setQuantity] = useState(null)
-  const[total,setTotal] = useState(null)
-  const[name,setName] = useState(null)
-  const [size,setSize] = useState(null)
-  const [sellerId,setSellerId] = useState(null)
   const [paymentInfo, setPaymentInfo] = useState({
     proId: null,
     userId: null,
@@ -26,96 +135,79 @@ function PaymentType() {
     size: null,
     sellerId: null,
   });
-  
 
   useEffect(() => {
-    // Move your data fetching logic into the useEffect hook
     const fetchData = async () => {
       const queryParams = new URLSearchParams(location.search);
-      setProId(queryParams.get('proId'));
-      setUserId(queryParams.get('userId'));
-      setCartId(queryParams.get('cartId'));
-      setQuantity(queryParams.get('quantity'));
-      setTotal(queryParams.get('total'));
-      setName(queryParams.get('name'));
-      setSize(queryParams.get('size'));
-      setSellerId(queryParams.get('sellerId'));
-
-
-
+      setPaymentInfo({
+        proId: queryParams.get('proId'),
+        userId: queryParams.get('userId'),
+        cartId: queryParams.get('cartId'),
+        quantity: queryParams.get('quantity'),
+        total: queryParams.get('total'),
+        name: queryParams.get('name'),
+        size: queryParams.get('size'),
+        sellerId: queryParams.get('sellerId'),
+      });
     };
 
-    fetchData(); // Call the fetchData function when the component mounts
+    fetchData();
   }, [location.search]);
-  useEffect(() => {
-    // Set the paymentInfo object based on the existing state variables
-    setPaymentInfo({
-      proId,
-      userId,
-      cartId,
-      quantity,
-      total,
-      name,
-      size,
-      sellerId,
+
+  const handleCodBuy = async () => {
+    const response = await axios.post('/Order/addorder', {
+      COD: 'COD',
+      ...paymentInfo,
     });
-  }, [proId, userId, cartId, quantity, total, name, size, sellerId]);
-
-//cod payment
-const handleCodBuy = async()=>{
-
-  const response = await axios.post('/Order/addorder',{
-    COD:'COD',
-    proId,
-    userId,
-    cartId,
-    quantity,
-    total,
-    name,
-    size,
-    sellerId
-
-  })
-  if(response.data.success){
-
-    navigate('/UserHome')
-  }else{
-    navigate('/cart')
-  
-  }
-}
-
-
-
-
+    if (response.data.success) {
+      navigate('/UserHome');
+    } else {
+      navigate('/cart');
+    }
+  };
 
   const handleOptionChange = (e) => {
     setSelectedOption(e.target.value);
   };
-  return (
-    <div>
-    <label>
-      <input
-        type="radio"
-        value="cod"
-        checked={selectedOption === 'cod'}
-        onChange={handleOptionChange}
-      />
-      Cod
-    </label>
 
-    <label>
-      <input
-        type="radio"
-        value="online"
-        checked={selectedOption === 'online'}
-        onChange={handleOptionChange}
-      />
-      Online
-    </label>
-    {selectedOption === 'online' ? (<OnlinePayment paymentInfo={paymentInfo} />):(<button onClick={handleCodBuy}>Buy COD</button>)}
-  </div>
-  )
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h4" gutterBottom>
+            Select Payment Method
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <RadioGroup value={selectedOption} onChange={handleOptionChange}>
+            <FormControlLabel
+              value="cod"
+              control={<Radio />}
+              label="Cash on Delivery (COD)"
+            />
+            <FormControlLabel
+              value="online"
+              control={<Radio />}
+              label="Online Payment"
+            />
+          </RadioGroup>
+        </Grid>
+        <Grid item xs={12}>
+          {selectedOption === 'online' ? (
+            <OnlinePayment paymentInfo={paymentInfo} />
+          ) : (
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleCodBuy}
+            >
+              Buy COD
+            </Button>
+          )}
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default PaymentType
+export default PaymentType;
--- client/src/pages/TestEnv.jsx
+++ client/src/pages/TestEnv.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { useNavigate,useLocation, Navigate } from 'react-router-dom';
+import { Button, Radio, FormControlLabel, RadioGroup, Typography, Container, Grid } from '@mui/material';
+import OnlinePayment from '../components/User/PaymentType/OnlinePayment';
 import TestOne from './TestOne'
 import axios from '../axios'
 import './Test.css'
@@ -7,75 +9,132 @@ import './Test.css'
 
 
 function TestEnv() {  
-  
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [emailError, setEmailError] = useState("")
-  const [passwordError, setPasswordError] = useState("")
-  
-  const onButtonClick = () => {
-
-    // Set initial error values to empty
-    setEmailError("")
-    setPasswordError("")
-
-    // Check if the user has entered both fields correctly
-    if ("" === email) {
-        setEmailError("Please enter your email")
-        return
-    }
-
-    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-        setEmailError("Please enter a valid email")
-        return
-    }
-
-    if ("" === password) {
-        setPasswordError("Please enter a password")
-        return
-    }
-
-    if (password.length < 7) {
-        setPasswordError("The password must be 8 characters or longer")
-        return
-    }
-
-    // Authentication calls will be made here...       
-
-}
+    const navigate = useNavigate();
+    const location = useLocation();
+     const [selectedOption, setSelectedOption] = useState('cod');
+     const[proId,setProId] = useState(null)
+     const[userId,setUserId] = useState(null)
+     const[cartId,setCartId] = useState(null)
+     const[quantity,setQuantity] = useState(null)
+     const[total,setTotal] = useState(null)
+     const[name,setName] = useState(null)
+     const [size,setSize] = useState(null)
+     const [sellerId,setSellerId] = useState(null)
+     const [paymentInfo, setPaymentInfo] = useState({
+       proId: null,
+       userId: null,
+       cartId: null,
+       quantity: null,
+       total: null,
+       name: null,
+       size: null,
+       sellerId: null,
+     });
+     
+   
+     useEffect(() => {
+       // Move your data fetching logic into the useEffect hook
+       const fetchData = async () => {
+         const queryParams = new URLSearchParams(location.search);
+         setProId(queryParams.get('proId'));
+         setUserId(queryParams.get('userId'));
+         setCartId(queryParams.get('cartId'));
+         setQuantity(queryParams.get('quantity'));
+         setTotal(queryParams.get('total'));
+         setName(queryParams.get('name'));
+         setSize(queryParams.get('size'));
+         setSellerId(queryParams.get('sellerId'));
+   
+   
+   
+       };
+   
+       fetchData(); // Call the fetchData function when the component mounts
+     }, [location.search]);
+     useEffect(() => {
+       // Set the paymentInfo object based on the existing state variables
+       setPaymentInfo({
+         proId,
+         userId,
+         cartId,
+         quantity,
+         total,
+         name,
+         size,
+         sellerId,
+       });
+     }, [proId, userId, cartId, quantity, total, name, size, sellerId]);
+   
+   //cod payment
+   const handleCodBuy = async()=>{
+   
+     const response = await axios.post('/Order/addorder',{
+       COD:'COD',
+       proId,
+       userId,
+       cartId,
+       quantity,
+       total,
+       name,
+       size,
+       sellerId
+   
+     })
+     if(response.data.success){
+   
+       navigate('/UserHome')
+     }else{
+       navigate('/cart')
+     
+     }
+   }
+   
+   
+   
+   
+   
+     const handleOptionChange = (e) => {
+       setSelectedOption(e.target.value);
+     };
+ 
 
   return (
-    <div className={"mainContainer"}>
-    <div className={"titleContainer"}>
-        <div>Login</div>
-    </div>
-    <br />
-    <div className={"inputContainer"}>
-        <input
-            value={email}
-            placeholder="Enter your email here"
-            onChange={ev => setEmail(ev.target.value)}
-            className={"inputBox"} />
-        <label className="errorLabel">{emailError}</label>
-    </div>
-    <br />
-    <div className={"inputContainer"}>
-        <input
-            value={password}
-            placeholder="Enter your password here"
-            onChange={ev => setPassword(ev.target.value)}
-            className={"inputBox"} />
-        <label className="errorLabel">{passwordError}</label>
-    </div>
-    <br />
-    <div className={"inputContainer"}>
-        <input
-            className={"inputButton"}
-            type="button"
-            onClick={onButtonClick}
-            value={"Log in"} />
-    </div>
-</div>
+    <Container>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography variant="h4" gutterBottom>
+          Select Payment Method
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <RadioGroup value={selectedOption} onChange={handleOptionChange}>
+          <FormControlLabel
+            value="cod"
+            control={<Radio />}
+            label="Cash on Delivery (COD)"
+          />
+          <FormControlLabel
+            value="online"
+            control={<Radio />}
+            label="Online Payment"
+          />
+        </RadioGroup>
+      </Grid>
+      <Grid item xs={12}>
+        {selectedOption === 'online' ? (
+          <OnlinePayment paymentInfo={paymentInfo} />
+        ) : (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleCodBuy}
+          >
+            Buy COD
+          </Button>
+        )}
+      </Grid>
+    </Grid>
+  </Container>
 
   )
 }
